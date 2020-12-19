bfbc0b3d51ab0f8781f82d32544442a4cd328d02
AdamPrzywuski <adamprzywuski@gmail.com>
2020-12-19 03:00:41 +0100

    tests

--- per_partner_simulator.py
+++ per_partner_simulator.py
@@ -20,21 +20,33 @@ class per_partner_simulator():
 
     def getting_value_of_averge_click_cost(self):
         for i in range(0, self.how_many_days):
-            self._getting_sales_on_click_per_day(i)
+            self._getting_sales_and_click(i)
         self.per_partner_averge_click_cost = (self.sales_amount_in_euro * 0.12) / self.numbers_of_clicks
         print("For the partnerId= " + self.partner_id + "\n the number of click was " + str(self.numbers_of_clicks)
               + "\n and the all sales_amount was " + str(self.sales_amount_in_euro) +
               "\n That means the CPM was " + str(self.per_partner_averge_click_cost))
 
     def all_info_about_partner(self):
-
         # print(self.how_many_days)
-
-        for i in range(0, self.how_many_days):
-            self._getting_sales_and_click(i)
+        self._first_time_getting_days()
+        for i in range(1, self.how_many_days):
+            self._getting_sales_on_click_per_day(i)
         with open("log.json", "w") as outfile:
             json.dump(self.json_score, outfile)
 
+    def _first_time_getting_days(self):
+        date_in_day = self.k.next_day(0)
+        self.day = date_in_day.iloc[1]["date"]
+        self._saving_to_json()
+        # here i should json data
+        productsOfDay = set()
+        for rows in date_in_day.itertuples(index=True):
+            productsOfDay.add(getattr(rows, "product_id"))
+            #self.productsSeenSoFar.add(getattr(rows, "product_id"))
+        # getting the new excluded day
+        #self.productsActuallyExcluded = productsOfDay.intersection(self.productsToExclude)
+        # self.productsSeenSoFar=self.productsSeenSoFar.join(productsOfDay)
+        self.go_to_optimizer(productsOfDay)
 
     def _getting_sales_and_click(self,i):
         date_in_day = self.k.next_day(i)
@@ -49,26 +61,21 @@ class per_partner_simulator():
                 self.sales_amount_in_euro = self.sales_amount_in_euro + money
 
     def _getting_sales_on_click_per_day(self, i):
+
         date_in_day = self.k.next_day(i)
         self.day = date_in_day.iloc[1]["date"]
         # here i should json data
-        self._saving_to_json()
+        #self._saving_to_json()
+        productsOfDay=set()
         for rows in date_in_day.itertuples(index=True):
-
-            # getting data from single days
-            sale = getattr(rows, "Sale")
-            money = getattr(rows, "SalesAmountInEuro")
-
+            productsOfDay.add(getattr(rows, "product_id"))
             self.productsSeenSoFar.add(getattr(rows, "product_id"))
-            if sale != -1:
-                self.numbers_of_clicks = self.numbers_of_clicks + 1
-            if money != -1:
-                self.sales_amount_in_euro = self.sales_amount_in_euro + money
         # getting the new excluded day
-
-        self.productsActuallyExcluded = self.productsSeenSoFar.intersection(self.productsToExclude)
-        self.productsSeenSoFar - self.productsActuallyExcluded
+        self.productsActuallyExcluded = productsOfDay.intersection(self.productsToExclude)
+        #self.productsSeenSoFar=self.productsSeenSoFar.join(productsOfDay)
+        self._saving_to_json()
         self.go_to_optimizer(self.productsSeenSoFar)
+
 
     def go_to_optimizer(self, list_):
         op = opp.optimizer(self.partner_id)
